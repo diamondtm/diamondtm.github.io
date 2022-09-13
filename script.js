c46cd55142eee8bed4cbaf7477b1dbc249c7b81a
Zsolt2132 <47487294+Zsolt2132@users.noreply.github.com>
2022-09-13 12:43:21 +0200

    a

---
+++ script.js
@@ -0,0 +1,32 @@
+	function Osszeadas()
+	{
+		var num1, num2, res;
+		num1=Number(document.calc.szam1.value);
+		num2=Number(document.calc.szam2.value);
+		res = num1+num2;
+		document.calc.eredmeny.value=res;
+	}
+	function Kivonas()
+	{
+		var num1, num2, res;
+		num1=Number(document.calc.szam1.value);
+		num2=Number(document.calc.szam2.value);
+		res = num1-num2;
+		document.calc.eredmeny.value=res;
+	}
+	function Szorzas()
+	{
+		var num1, num2, res;
+		num1=Number(document.calc.szam1.value);
+		num2=Number(document.calc.szam2.value);
+		res = num1*num2;
+		document.calc.eredmeny.value=res;
+	}
+	function Osztas()
+	{
+		var num1, num2, res;
+		num1=Number(document.calc.szam1.value);
+		num2=Number(document.calc.szam2.value);
+		res = num1/num2;
+		document.calc.eredmeny.value=res;
+	}
