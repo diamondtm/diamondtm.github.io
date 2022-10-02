9e01dcb6fe84264089f9c9d830bd5e0c0972070e
Zsolt2132 <47487294+Zsolt2132@users.noreply.github.com>
2022-10-02 19:25:33 +0200

    hiq

---
+++ save/save3/script.js
@@ -0,0 +1,54 @@
+	function Osszeadas()
+	{
+		var num1, num2, res;
+		num1=Number(document.calc.szam1.value);
+		num2=Number(document.calc.szam2.value);
+		res = num1+num2;
+		document.calc.eredmeny.value=res;
+	}
+	function Kivonas()
+	{
+		var num1, num2, res;
+		num1=Number(document.calc.szam1.value);
+		num2=Number(document.calc.szam2.value);
+		res = num1-num2;
+		document.calc.eredmeny.value=res;
+	}
+	function Szorzas()
+	{
+		var num1, num2, res;
+		num1=Number(document.calc.szam1.value);
+		num2=Number(document.calc.szam2.value);
+		res = num1*num2;
+		document.calc.eredmeny.value=res;
+	}
+	function Osztas()
+	{
+		var num1, num2, res;
+		num1=Number(document.calc.szam1.value);
+		num2=Number(document.calc.szam2.value);
+		res = num1/num2;
+		document.calc.eredmeny.value=res;
+	}
+	function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
+var cards = document.querySelectorAll('.card');
+
+[...cards].forEach((card)=>{
+  card.addEventListener( 'click', function() {
+    card.classList.toggle('hatlap');
+	delay(4000).then(() => card.classList.remove('hatlap'));
+	
+  });
+ 
+});
+
+function myFunction() {
+   var tabi = document.getElementById('tablas');
+   tabi.classList.toggle('yep');
+};
+
+
+
